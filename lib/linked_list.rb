fcf9fb9b1d657a60f44e01d24bd0c55cb52a0205
Elom Amouzou <elom1990@yahoo.com>
2020-05-07 00:49:25 -0600

    add find, pop, includes? methods + found-value? and remove_node helpers

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -71,4 +71,46 @@ class LinkedList
     node.next_node = new_node
   end
 
+  def find(first_position, elements = 1)
+    return nil if is_empty?
+    node = @head
+    first_position.times { node = node.next_node }
+    output_string = "The #{node.surname} family"
+    until tail?(node) == true || elements == 1
+      elements -= 1
+      node = node.next_node
+      output_string << ", followed by the #{node.surname} family"
+    end
+    output_string
+  end
+
+  def includes?(given_value)
+    return false if is_empty? == true
+    found_value?(given_value) if is_empty? == false
+  end
+
+  #recursion example
+  def found_value?(given_value, node = @head)
+    return true if node.surname == given_value
+    return false if tail?(node)
+    found_value?(given_value, node.next_node)
+  end
+
+  def pop
+    node = @head
+    node_count ||= count
+    return nil if is_empty?
+    return remove_node(node.next_node) if node_count == 1
+    (node_count - 2).times { node = node.next_node }
+    return remove_node(node) if node_count != 1
+  end
+
+  def remove_node(given_node)
+    final_node = given_node.next_node
+    family_name = final_node.surname
+    given_node.next_node = nil
+    puts "The #{family_name} family has died of dysentery"
+    final_node
+  end
+
 end
